f4b705a923854e56436f457f6ff07c8e092926f8
x-io Technologies <social@x-io.co.uk>
2022-08-05 15:21:23 +0100

    Increment version

--- x-IMU3-API/CSharp/x-IMU3/AssemblyInfo.cpp
+++ x-IMU3-API/CSharp/x-IMU3/AssemblyInfo.cpp
@@ -13,7 +13,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
 
-[assembly:AssemblyVersionAttribute("0.6.0")];
+[assembly:AssemblyVersionAttribute("0.7.0")];
 
 [assembly:ComVisible(false)];
 
