1ad2ff8bfae0a23d19334879a1410e062a4abafc
Jelle Bakker <jellebakker@jb-audio.com>
2022-08-18 18:11:16 +0200

    Fix order of properties in setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ if len(sys.argv) == 1:  # if this script was called without arguments
     sys.argv.append("install")
     sys.argv.append("--user")
 
+github_url = "https://github.com/xioTechnologies/x-IMU3-Software"
+
 if platform.system() == "Darwin":
     os.environ["LDFLAGS"] = "-framework cocoa -framework IOKit"
     libraries = ["ximu3"]
@@ -26,18 +28,16 @@ ext_modules = Extension("ximu3",
                         libraries=libraries,
                         define_macros=[("_CRT_SECURE_NO_WARNINGS", "")])
 
-github_url = "https://github.com/xioTechnologies/x-IMU3-Software"
-
 setup(name="ximu3",
       version="0.8.0",
-      author="x-io Technologies Limited",
-      author_email="info@x-io.co.uk",
-      url=github_url,
       description="x-IMU3 API",
       long_description="See [github](" + github_url + ") for documentation and examples.",
       long_description_content_type='text/markdown',
+      url=github_url,
+      author="x-io Technologies Limited",
+      author_email="info@x-io.co.uk",
       license="MIT",
-      ext_modules=[ext_modules],
       classifiers=["Programming Language :: Python :: 3.8",
                    "Programming Language :: Python :: 3.9",
-                   "Programming Language :: Python :: 3.10"])  # versions shown by pyversions badge in README
+                   "Programming Language :: Python :: 3.10"],  # versions shown by pyversions badge in README
+      ext_modules=[ext_modules])
