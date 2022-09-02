8a675009fe1bd163a41d7673cb246ba562ca7460
Jelle Bakker <jellebakker@jb-audio.com>
2022-09-02 17:56:54 +0200

    Implement network announcement in Python

--- Examples/Python/tcp_connection.py
+++ Examples/Python/tcp_connection.py
@@ -5,13 +5,13 @@ import ximu3
 if helpers.yes_or_no("Search for connections?"):
     print("Searching for connections")
 
-    devices = ximu3.NetworkDiscovery.scan(2000)
+    messages = ximu3.NetworkAnnouncement().get_messages_after_short_delay()
 
-    if not devices:
+    if not messages:
         raise Exception("No TCP connections available")
 
-    print("Found " + devices[0].device_name + " - " + devices[0].serial_number)
+    print("Found " + messages[0].device_name + " - " + messages[0].serial_number)
 
-    connection.run(devices[0].tcp_connection_info)
+    connection.run(messages[0].tcp_connection_info)
 else:
     connection.run(ximu3.TcpConnectionInfo("192.168.1.1", 7000))
