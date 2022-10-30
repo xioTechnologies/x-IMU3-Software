45a84cb588539fb937633692d80b875218817735
x-io Technologies <social@x-io.co.uk>
2022-10-30 13:36:58 +0000

    Increment version

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-version = "1.0.1"
+version = "1.0.2"
 
 
 def replace(file_path, string):
--- x-IMU3-API/CSharp/x-IMU3/AssemblyInfo.cpp
+++ x-IMU3-API/CSharp/x-IMU3/AssemblyInfo.cpp
@@ -13,7 +13,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
 
-[assembly:AssemblyVersionAttribute("1.0.1")];
+[assembly:AssemblyVersionAttribute("1.0.2")];
 
 [assembly:ComVisible(false)];
 
