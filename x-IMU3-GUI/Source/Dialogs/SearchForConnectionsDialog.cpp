05c1f44082cf873123d2dbd1b4449b57f68cceda
Jelle Bakker <jellebakker@jb-audio.com>
2023-02-04 16:05:58 +0100

    Implement for GUI

--- x-IMU3-GUI/Source/Dialogs/SearchForConnectionsDialog.cpp
+++ x-IMU3-GUI/Source/Dialogs/SearchForConnectionsDialog.cpp
@@ -94,11 +94,11 @@ void SearchForConnectionsDialog::timerCallback()
     {
         if (ApplicationSettings::getSingleton().searchUdp)
         {
-            addConnection(message.device_name, message.serial_number, std::make_shared<ximu3::UdpConnectionInfo>(message.udp_connection_info), ximu3::XIMU3_ConnectionTypeUdp);
+            addConnection(message.device_name, message.serial_number, std::make_shared<ximu3::UdpConnectionInfo>(ximu3::XIMU3_network_announcement_message_to_udp_connection_info(message)), ximu3::XIMU3_ConnectionTypeUdp);
         }
         if (ApplicationSettings::getSingleton().searchTcp)
         {
-            addConnection(message.device_name, message.serial_number, std::make_shared<ximu3::TcpConnectionInfo>(message.tcp_connection_info), ximu3::XIMU3_ConnectionTypeTcp);
+            addConnection(message.device_name, message.serial_number, std::make_shared<ximu3::TcpConnectionInfo>(ximu3::XIMU3_network_announcement_message_to_tcp_connection_info(message)), ximu3::XIMU3_ConnectionTypeTcp);
         }
     }
 
