5d6311cf491d0effb7d8e00196a13bca688df26c
x-io Technologies <social@x-io.co.uk>
2023-05-07 14:27:38 +0100

    Improve vertical whitespace in C examples

--- Examples/C/Examples/SerialConnection.c
+++ Examples/C/Examples/SerialConnection.c
@@ -8,15 +8,20 @@ void SerialConnection()
     if (AskQuestion("Search for connections?"))
     {
         printf("Searching for connections\n");
+
         const XIMU3_Devices devices = XIMU3_port_scanner_scan_filter(XIMU3_ConnectionTypeSerial);
+
         if (devices.length == 0)
         {
             printf("No serial connections available\n");
             return;
         }
         printf("Found %s - %s\n", devices.array[0].device_name, devices.array[0].serial_number);
+
         const XIMU3_SerialConnectionInfo connectionInfo = devices.array[0].serial_connection_info;
+
         XIMU3_devices_free(devices);
+
         Run(XIMU3_connection_new_serial(connectionInfo), XIMU3_serial_connection_info_to_string(connectionInfo));
     }
     else
--- Examples/C/Examples/UsbConnection.c
+++ Examples/C/Examples/UsbConnection.c
@@ -8,15 +8,20 @@ void UsbConnection()
     if (AskQuestion("Search for connections?"))
     {
         printf("Searching for connections\n");
+
         const XIMU3_Devices devices = XIMU3_port_scanner_scan_filter(XIMU3_ConnectionTypeUsb);
+
         if (devices.length == 0)
         {
             printf("No USB connections available\n");
             return;
         }
         printf("Found %s - %s\n", devices.array[0].device_name, devices.array[0].serial_number);
+
         const XIMU3_UsbConnectionInfo connectionInfo = devices.array[0].usb_connection_info;
+
         XIMU3_devices_free(devices);
+
         Run(XIMU3_connection_new_usb(connectionInfo), XIMU3_usb_connection_info_to_string(connectionInfo));
     }
     else
