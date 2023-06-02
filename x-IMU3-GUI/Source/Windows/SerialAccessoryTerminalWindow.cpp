bcceedb5cddaab365e18101da5cb91df39411625
Jelle Bakker <jellebakker@jb-audio.com>
2023-06-02 12:28:54 +0200

    Rename Serial History

--- x-IMU3-GUI/Source/Windows/SerialAccessoryTerminalWindow.cpp
+++ x-IMU3-GUI/Source/Windows/SerialAccessoryTerminalWindow.cpp
@@ -28,22 +28,22 @@ SerialAccessoryTerminalWindow::SerialAccessoryTerminalWindow(const juce::ValueTr
             sendButton.setToggleState(responses.empty(), juce::dontSendNotification);
         });
 
-        for (const auto send : sendHistory)
+        for (const auto serial : serialHistory)
         {
-            if (send.getProperty("send") == sendValue.getText())
+            if (serial.getProperty("serial") == sendValue.getText())
             {
-                sendHistory.removeChild(send, nullptr);
+                serialHistory.removeChild(serial, nullptr);
                 break;
             }
         }
 
-        while (sendHistory.getNumChildren() > 9)
+        while (serialHistory.getNumChildren() > 9)
         {
-            sendHistory.removeChild(sendHistory.getChild(sendHistory.getNumChildren() - 1), nullptr);
+            serialHistory.removeChild(serialHistory.getChild(serialHistory.getNumChildren() - 1), nullptr);
         }
 
-        sendHistory.addChild({ "Send", {{ "send", sendValue.getText() }}}, 0, nullptr);
-        file.replaceWithText(sendHistory.toXmlString());
+        serialHistory.addChild({ "Serial", {{ "serial", sendValue.getText() }}}, 0, nullptr);
+        file.replaceWithText(serialHistory.toXmlString());
 
         loadSendHistory();
     };
@@ -146,16 +146,16 @@ juce::String SerialAccessoryTerminalWindow::removeEscapeCharacters(const juce::S
 
 void SerialAccessoryTerminalWindow::loadSendHistory()
 {
-    sendHistory = juce::ValueTree::fromXml(file.loadFileAsString());
-    if (sendHistory.isValid() == false)
+    serialHistory = juce::ValueTree::fromXml(file.loadFileAsString());
+    if (serialHistory.isValid() == false)
     {
-        sendHistory = juce::ValueTree("SendHistory");
+        serialHistory = juce::ValueTree("SerialHistory");
     }
 
     sendValue.clear(juce::dontSendNotification);
-    for (const auto send : sendHistory)
+    for (const auto send : serialHistory)
     {
-        sendValue.addItem(send.getProperty("send"), sendValue.getNumItems() + 1);
+        sendValue.addItem(send.getProperty("serial"), sendValue.getNumItems() + 1);
     }
 
     sendValue.setText(sendValue.getNumItems() > 0 ? sendValue.getItemText(0) : "Hello World!", juce::dontSendNotification);
