380183a938b9409b0fb13b10080b80d1dba6538e
x-io Technologies <social@x-io.co.uk>
2023-09-01 17:38:21 +0100

    Update bootloader for v2 hardware

--- x-IMU3-GUI/Source/Dialogs/UpdatingFirmwareDialog.cpp
+++ x-IMU3-GUI/Source/Dialogs/UpdatingFirmwareDialog.cpp
@@ -1,5 +1,6 @@
 #include "../ApplicationSettings.h"
 #include "../Firmware/Firmware.h"
+#include "CommandMessage.h"
 #include "ErrorDialog.h"
 #include "UpdatingFirmwareDialog.h"
 #include "Ximu3Bootloader.h"
@@ -17,11 +18,11 @@ UpdatingFirmwareDialog::UpdatingFirmwareDialog(std::unique_ptr<ximu3::Connection
 
     juce::Thread::launch([&]
                          {
-                             const auto showError = [&]
+                             const auto showError = [&](const auto error)
                              {
-                                 juce::MessageManager::callAsync([&]
+                                 juce::MessageManager::callAsync([&, error]
                                                                  {
-                                                                     DialogQueue::getSingleton().pushFront(std::make_unique<ErrorDialog>("Firmware update failed."));
+                                                                     DialogQueue::getSingleton().pushFront(std::make_unique<ErrorDialog>(error));
                                                                      DialogQueue::getSingleton().pop();
                                                                  });
                              };
@@ -40,7 +41,25 @@ UpdatingFirmwareDialog::UpdatingFirmwareDialog(std::unique_ptr<ximu3::Connection
                              auto connection = std::make_unique<ximu3::Connection>(*connectionInfo);
                              if (connection->open() != ximu3::XIMU3_ResultOk)
                              {
-                                 showError();
+                                 showError("Unable to open connection.");
+                                 return;
+                             }
+
+                             // Read hardware version
+                             const auto responses = connection->sendCommands({ "{\"hardwareVersion\":null}" }, ApplicationSettings::getSingleton().commands.retries, ApplicationSettings::getSingleton().commands.timeout);
+                             if (responses.empty())
+                             {
+                                 showError("Unable to read hardware version.");
+                                 return;
+                             }
+                             const auto hardwareVersion = CommandMessage(responses[0]).value.toString();
+
+                             // Check compatibility
+                             const auto firmwareIsV2 = juce::File(fileName).getFileName().startsWith("x-IMU3-Firmware-v2.");
+                             const auto hardwareIsV2 = hardwareVersion.startsWith("v2.");
+                             if (firmwareIsV2 ^ hardwareIsV2)
+                             {
+                                 showError("The detected " + hardwareVersion + " hardware is " + (firmwareIsV2 ? "not" : "only") + " compatible with v2.x.x firmware.");
                                  return;
                              }
 
@@ -48,7 +67,7 @@ UpdatingFirmwareDialog::UpdatingFirmwareDialog(std::unique_ptr<ximu3::Connection
                              updateProgress("Sending Bootloader Command");
                              if (connection->sendCommands({ "{\"bootloader\":null}" }, ApplicationSettings::getSingleton().commands.retries, ApplicationSettings::getSingleton().commands.timeout).empty())
                              {
-                                 showError();
+                                 showError("Unable to confirm bootloader command.");
                                  return;
                              }
 
@@ -63,7 +82,7 @@ UpdatingFirmwareDialog::UpdatingFirmwareDialog(std::unique_ptr<ximu3::Connection
                              {
                                  updateProgress("Attempting Upload on " + portName);
 
-                                 if (XIMU3_upload_firmware("PIC32MZ2048EFG124", fileName.toRawUTF8(), portName.data()) == 0)
+                                 if (XIMU3_upload_firmware(hardwareIsV2 ? "PIC32MZ2048EFG100" : "PIC32MZ2048EFG124", fileName.toRawUTF8(), portName.data()) == 0)
                                  {
                                      updateProgress("Update Complete", true);
                                      juce::Timer::callAfterDelay(1000, [&]
@@ -74,7 +93,7 @@ UpdatingFirmwareDialog::UpdatingFirmwareDialog(std::unique_ptr<ximu3::Connection
                                  }
                              }
 
-                             showError();
+                             showError("Firmware update failed.");
                          });
 }
 
