51b11b56d37ad9187180d100827f0110b45a5e93
x-io Technologies <social@x-io.co.uk>
2024-09-19 13:52:43 +0100

    Change get char

--- Examples/CSharp/x-IMU3-Examples/Helpers.cs
+++ Examples/CSharp/x-IMU3-Examples/Helpers.cs
@@ -4,11 +4,14 @@ namespace Ximu3Examples
 {
     class Helpers
     {
-        public static Char GetKey()
+        public static Char? GetKey()
         {
-            Char key = Char.ToUpper(Console.ReadKey().KeyChar);
-            Console.ReadLine();
-            return key;
+            string buffer = Console.ReadLine();
+            if (string.IsNullOrEmpty(buffer))
+            {
+                return null;
+            }
+            return Char.ToUpper(buffer[0]);
         }
 
         public static bool AskQuestion(string question)
