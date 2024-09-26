8a2ee3ddb673bfa7d76d7f192cbd3f4e2acf261c
x-io Technologies <social@x-io.co.uk>
2024-09-26 12:45:27 +0100

    Implement for Rust API and examples only

--- x-IMU3-API/Rust/src/connections/generic_connection.rs
+++ x-IMU3-API/Rust/src/connections/generic_connection.rs
@@ -1,12 +1,14 @@
 use crossbeam::channel::Sender;
 use std::sync::{Arc, Mutex};
 use crate::connection_info::*;
+use crate::connection_status::*;
 use crate::decoder::*;
 
 pub trait GenericConnection {
     fn open(&mut self) -> std::io::Result<()>;
     fn close(&self);
     fn get_info(&self) -> ConnectionInfo;
+    fn get_status(&self) -> ConnectionStatus;
     fn get_decoder(&self) -> Arc<Mutex<Decoder>>;
     fn get_write_sender(&self) -> Option<Sender<String>>;
 }
