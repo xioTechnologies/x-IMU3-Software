b1e096901e8180ebdc0715cf61698bbcac0034ff
x-io Technologies <social@x-io.co.uk>
2025-05-18 17:22:00 +0100

    Add open async examples

---
+++ Examples/Rust/src/examples/open_async.rs
@@ -0,0 +1,30 @@
+use ximu3::connection::*;
+use ximu3::port_scanner::*;
+
+pub fn run() {
+    // Search for connection
+    let devices = PortScanner::scan();
+
+    if devices.is_empty() {
+        println!("No connections available");
+        return;
+    }
+
+    let device = devices.first().unwrap();
+
+    println!("Found {} {}", device.device_name, device.serial_number);
+
+    // Open connection
+    let connection = Connection::new(&device.connection_info);
+
+    let closure = Box::new(|result| match result {
+        Ok(_) => println!("Ok"),
+        Err(_) => println!("Error"),
+    });
+
+    connection.open_async(closure);
+
+    // Close connection
+    std::thread::sleep(std::time::Duration::from_secs(3));
+    connection.close();
+}
