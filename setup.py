50b42b4b72562d13282ab410948438247bb69211
x-io Technologies <social@x-io.co.uk>
2025-06-11 16:27:25 +0100

    Add Python stub file

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import platform
-from setuptools import setup, Extension
+
+from setuptools import Extension, setup
 
 if platform.system() == "Darwin":
     os.environ["LDFLAGS"] = "-framework cocoa -framework IOKit"
@@ -10,10 +11,16 @@ elif platform.system() == "Linux":
 else:
     libraries = ["ximu3", "ws2_32", "userenv", "setupapi", "advapi32"]
 
-ext_modules = Extension("ximu3",
-                        ["x-IMU3-API/Python/Python-C-API/ximu3.c"],
-                        library_dirs=["x-IMU3-API/Rust/target/release"],
-                        libraries=libraries,
-                        define_macros=[("_CRT_SECURE_NO_WARNINGS", "")])
+ext_modules = Extension(
+    "ximu3",
+    ["x-IMU3-API/Python/Python-C-API/ximu3.c"],
+    library_dirs=["x-IMU3-API/Rust/target/release"],
+    libraries=libraries,
+    define_macros=[("_CRT_SECURE_NO_WARNINGS", "")],
+)
 
-setup(ext_modules=[ext_modules])
+setup(
+    ext_modules=[ext_modules],
+    packages=["ximu3-stubs"],
+    package_dir={"ximu3-stubs": "x-IMU3-API/Python/ximu3-stubs"},
+)
