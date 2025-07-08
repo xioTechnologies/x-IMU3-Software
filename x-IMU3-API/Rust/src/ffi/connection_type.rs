d013701efb0785e68aeb0f734a08b0980cbbe3be
x-io Technologies <social@x-io.co.uk>
2025-07-08 10:57:19 +0100

    Only C API should use ConnectionType

--- x-IMU3-API/Rust/src/ffi/connection_type.rs
+++ x-IMU3-API/Rust/src/ffi/connection_type.rs
@@ -1,6 +1,44 @@
-use crate::connection_type::*;
+use crate::connection_info::*;
 use crate::ffi::helpers::*;
+use std::fmt;
 use std::os::raw::c_char;
+
+#[repr(C)]
+#[derive(Clone, Copy, PartialEq)]
+pub enum ConnectionType {
+    Usb,
+    Serial,
+    Tcp,
+    Udp,
+    Bluetooth,
+    File,
+}
+
+impl From<&ConnectionInfo> for ConnectionType {
+    fn from(connection_info: &ConnectionInfo) -> Self {
+        match connection_info {
+            ConnectionInfo::UsbConnectionInfo(_) => ConnectionType::Usb,
+            ConnectionInfo::SerialConnectionInfo(_) => ConnectionType::Serial,
+            ConnectionInfo::TcpConnectionInfo(_) => ConnectionType::Tcp,
+            ConnectionInfo::UdpConnectionInfo(_) => ConnectionType::Udp,
+            ConnectionInfo::BluetoothConnectionInfo(_) => ConnectionType::Bluetooth,
+            ConnectionInfo::FileConnectionInfo(_) => ConnectionType::File,
+        }
+    }
+}
+
+impl fmt::Display for ConnectionType {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ConnectionType::Usb => write!(formatter, "USB"),
+            ConnectionType::Serial => write!(formatter, "Serial"),
+            ConnectionType::Tcp => write!(formatter, "TCP"),
+            ConnectionType::Udp => write!(formatter, "UDP"),
+            ConnectionType::Bluetooth => write!(formatter, "Bluetooth"),
+            ConnectionType::File => write!(formatter, "File"),
+        }
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn XIMU3_connection_type_to_string(connection_type: ConnectionType) -> *const c_char {
