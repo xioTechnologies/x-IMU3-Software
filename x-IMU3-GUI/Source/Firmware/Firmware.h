c8de6f5805a32134e8863990f6442f1d941e7930
x-io Technologies <social@x-io.co.uk>
2025-11-04 12:51:30 +0000

    Update firmware

--- x-IMU3-GUI/Source/Firmware/Firmware.h
+++ x-IMU3-GUI/Source/Firmware/Firmware.h
@@ -5,7 +5,7 @@
 
 namespace Firmware
 {
-    static const juce::String hexFile = "x-IMU3-Firmware-v2.5.3.hex";
-    static const juce::String version = "v2.5.3";
-    static const juce::MemoryBlock memoryBlock { BinaryData::xIMU3Firmwarev2_5_3_hex, BinaryData::xIMU3Firmwarev2_5_3_hexSize };
+    static const juce::String hexFile = "x-IMU3-Firmware-v2.5.4.hex";
+    static const juce::String version = "v2.5.4";
+    static const juce::MemoryBlock memoryBlock { BinaryData::xIMU3Firmwarev2_5_4_hex, BinaryData::xIMU3Firmwarev2_5_4_hexSize };
 }
