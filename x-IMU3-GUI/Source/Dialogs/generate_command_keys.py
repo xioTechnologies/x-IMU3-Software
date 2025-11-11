f4b311aec9646c0a38cba16f9cd58b87093fb1ff
x-io Technologies <social@x-io.co.uk>
2025-11-11 11:41:00 +0000

    Update commands

--- x-IMU3-GUI/Source/Dialogs/generate_command_keys.py
+++ x-IMU3-GUI/Source/Dialogs/generate_command_keys.py
@@ -14,21 +14,38 @@ NULL = "4"
 settings_keys = []
 settings_types = []
 
-with open("../DeviceSettings/DeviceSettings.json") as file:
-    for setting in json.load(file):
-        if bool(setting.get("calibration")):
+with open("../DeviceSettings/Settings.json") as file:
+    for setting in json.load(file)["settings"]:
+        name = helpers.snake_case(setting["name"])
+
+        if name in (
+            "calibration_date",
+            "gyroscope_misalignment",
+            "gyroscope_sensitivity",
+            "gyroscope_offset",
+            "accelerometer_misalignment",
+            "accelerometer_sensitivity",
+            "accelerometer_offset",
+            "soft_iron_matrix",
+            "hard_iron_offset",
+            "high_g_accelerometer_misalignment",
+            "high_g_accelerometer_sensitivity",
+            "high_g_accelerometer_offset",
+        ):
             continue
 
-        settings_keys.append(helpers.snake_case(setting["name"]))
+        settings_keys.append(name)
 
-        if any([d in setting["declaration"] for d in ["char name[", "RS9116BDAddress", "RS9116IPAddress", "RS9116LinkKey", "RS9116MacAddress"]]):
+        declaration = setting["declaration"]
+
+        if any(d in declaration for d in ("char name[", "RS9116BDAddress", "RS9116IPAddress", "RS9116LinkKey", "RS9116MacAddress")):
             settings_types.append(STRING)
-        elif any([d in setting["declaration"] for d in ["float", "uint32_t", "int32_t", "uint16_t"]]):
+        elif any(d in declaration for d in ("float", "uint32_t", "int32_t", "uint16_t")):
             settings_types.append(NUMBER)
-        elif "bool" in setting["declaration"]:
+        elif "bool" in declaration:
             settings_types.append(TRUE)
         else:
-            json_type = setting["declaration"].split()[0]
+            json_type = declaration.split()[0]
             settings_types.append(NUMBER)
 
 with open("CommandKeys.xml", "w") as file:
@@ -51,6 +68,7 @@ with open("CommandKeys.xml", "w") as file:
     file.write(f'    <Command key="heading" type="{NUMBER}"/>\n')
     file.write(f'    <Command key="accessory" type="{STRING}"/>\n')
     file.write(f'    <Command key="note" type="{STRING}"/>\n')
+    file.write(f'    <Command key="timestamp" type="{NUMBER}"/>\n')
     file.write(f'    <Command key="format" type="{NULL}"/>\n')
     file.write(f'    <Command key="test" type="{NULL}"/>\n')
     file.write(f'    <Command key="bootloader" type="{NULL}"/>\n')
