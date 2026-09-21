b2674afbefc04b2609f0abd05131122d9796183e
Jelle Bakker <jellebakker@jb-audio.com>
2026-09-21 16:50:08 +0200

    Fix tooltips appearing behind dialogs

--- x-IMU3-GUI/Source/Dialogs/Dialog.h
+++ x-IMU3-GUI/Source/Dialogs/Dialog.h
@@ -56,13 +56,6 @@ private:
 
     int minimumWidth = 0;
 
-    juce::TimedCallback timer
-    {
-        [&] {
-            getTopLevelComponent()->setAlwaysOnTop(juce::Process::isForegroundProcess());
-        }
-    };
-
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Dialog)
 };
 
